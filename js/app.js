38e6f1a101d7cf587418ad47f4b29ce6ed13455f
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-02-11 18:48:26 -0800

    updated display and score function

--- js/app.js
+++ js/app.js
@@ -7,13 +7,18 @@ var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "Kin
 var deck = [];
 var player = [];
 var dealer = [];
-var playerBoxScore;
-var dealerBoxScore;
+
+var playerHandEl = document.getElementById("player-hand");
+var dealerHandEl = document.getElementById("dealer-hand");
+var playerScoreEl = document.getElementById("player-score");
+var dealerScoreEl = document.getElementById("dealer-score");
+
 var softHand;
 // document buttons
 var hitBtn = document.getElementById('hit-btn');
 var standBtn = document.getElementById('stand-btn');
 var newGameBtn = document.getElementById('newgame-btn');
+
 
 
 suits.forEach( function(suit) {
@@ -54,16 +59,20 @@ function deal(who, numCards) {
         }
     }
 
+    displayPlayerCards();
+
 
     
     function dealerTurn() {
         //softHand;
         // show hidden card
         let dealerScore = handScore(dealer);
+        console.log(dealerScore)
         while (dealerScore < 17) {
             let drawCard = deck.pop();
             dealer.push(drawCard);
             dealerScore = handScore(dealer);
+            console.log(dealerScore)
             if (dealerScore > 21) {
                 console.log("BUST!")
                 break;
@@ -73,6 +82,7 @@ function deal(who, numCards) {
     
     function hit() {
         player.push(deck.pop());
+        displayPlayerCards();
         console.log('new player hand ' + player)
         if ((player[0].value + player[1].value) > 21) {
             console.log("BUST!");
@@ -91,10 +101,10 @@ function deal(who, numCards) {
     function handScore(deck) {
         let aceCount = 0;
         let score = 0;
-        for (let i = 0; i < deck.length(); i++) {
-            let card = deck.get(i);
+        for (let i = 0; i < deck.length; i++) {
+            let card = deck[i];
             score += card.value;
-            if (card.ace())
+            if (card.rank === "Ace")
             ++aceCount;
         }
         while (score <= 11 && aceCount > 0) {
@@ -103,11 +113,35 @@ function deal(who, numCards) {
         }
         return score;
     }
+
+
+    function displayPlayerCards() {
+        playerHandEl.textContent = "";
+        player.forEach(function(card) {
+            let newCard = document.createElement("img");
+            newCard.src = card.src;
+            playerHandEl.appendChild(newCard);
+        });
+    }
+
+    function displayPlayerScore() {
+        playerScoreEl.textContent = "";
+
+    }
+   
+
+    dealer.forEach(function(card) {
+        let newCard = document.createElement("img");
+        newCard.src = card.src;
+        dealerHandEl.appendChild(newCard);
+    })
+
+   
     
-    document.getElementById("player-hand").textContent = player;
-    document.getElementById("dealer-hand").textContent = dealer;
-    document.getElementById("player-score").textContent = playerBoxScore;
-    document.getElementById("dealer-score").textContent = dealerBoxScore;
+    // document.getElementById("player-hand").textContent = player;
+    // document.getElementById("dealer-hand").textContent = dealer;
+    // document.getElementById("player-score").textContent = playerBoxScore;
+    // document.getElementById("dealer-score").textContent = dealerBoxScore;
 
     hitBtn.addEventListener('click', hit);
     standBtn.addEventListener('click', stand);
